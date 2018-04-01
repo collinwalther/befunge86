27e8c13d5a81f924fa47a2582a8d1245852f313b
Collin Walther <collinwalther@hotmail.com>
2018-04-01 01:36:42 -0700

    added unit test for Board

--- tst/BoardTest.cpp
+++ tst/BoardTest.cpp
@@ -1,3 +1,27 @@
+#include "Board.h"
+#include <vector>
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
+int Test3by3Matrix() {
+    char row0[] = {'1', '2', '3'};
+    char row1[] = {'4', '5', '6'};
+    char row2[] = {'7', '8', '9'};
+    char *squares[] = {row0, row1, row2};
+    Board b(squares, 3, 3);
+
+    vector<Cell *> expectedRow = b.GetRowAt(0);
+    assert(expectedRow[0]->GetChar() == '1');
+    assert(expectedRow[1]->GetChar() == '2');
+    assert(expectedRow[2]->GetChar() == '3');
+
+    return 0;
+}
+
 int main() {
+    Test3by3Matrix();
+   
     return 0;
 }
