5947e1d1287dcf4c58e7b69dfbc0f8343715e9d5
Collin Walther <collinwalther@hotmail.com>
2018-04-01 11:39:32 -0700

    added more tests for Board

--- tst/BoardTest.cpp
+++ tst/BoardTest.cpp
@@ -12,16 +12,191 @@ int Test3by3Matrix() {
     char *squares[] = {row0, row1, row2};
     Board b(squares, 3, 3);
 
-    vector<Cell *> expectedRow = b.GetRowAt(0);
-    assert(expectedRow[0]->GetChar() == '1');
-    assert(expectedRow[1]->GetChar() == '2');
-    assert(expectedRow[2]->GetChar() == '3');
+    vector<Cell *> e = b.GetRowAt(0);
+    assert(e[0]->GetChar() == '1');
+    assert(e[1]->GetChar() == '2');
+    assert(e[2]->GetChar() == '3');
+    assert(e.size() == 3);
+
+    e = b.GetRowAt(1);
+    assert(e[0]->GetChar() == '4');
+    assert(e[1]->GetChar() == '5');
+    assert(e[2]->GetChar() == '6');
+    assert(e.size() == 3);
+
+    e = b.GetRowAt(2);
+    assert(e[0]->GetChar() == '7');
+    assert(e[1]->GetChar() == '8');
+    assert(e[2]->GetChar() == '9');
+    assert(e.size() == 3);
+
+    e = b.GetColumnAt(0);
+    assert(e[0]->GetChar() == '1');
+    assert(e[1]->GetChar() == '4');
+    assert(e[2]->GetChar() == '7');
+    assert(e.size() == 3);
+
+    e = b.GetColumnAt(1);
+    assert(e[0]->GetChar() == '2');
+    assert(e[1]->GetChar() == '5');
+    assert(e[2]->GetChar() == '8');
+    assert(e.size() == 3);
+
+    e = b.GetColumnAt(2);
+    assert(e[0]->GetChar() == '3');
+    assert(e[1]->GetChar() == '6');
+    assert(e[2]->GetChar() == '9');
+    assert(e.size() == 3);
+
+    return 0;
+}
+
+int Test3by3MatrixWithHole() {
+    char row0[] = {'1', '2', '3'};
+    char row1[] = {'4', ' ', '6'};
+    char row2[] = {'7', '8', '9'};
+    char *squares[] = {row0, row1, row2};
+    Board b(squares, 3, 3);
+
+    vector<Cell *> e = b.GetRowAt(0);
+    assert(e[0]->GetChar() == '1');
+    assert(e[1]->GetChar() == '2');
+    assert(e[2]->GetChar() == '3');
+    assert(e.size() == 3);
+
+    e = b.GetRowAt(1);
+    assert(e[0]->GetChar() == '4');
+    assert(e[1]->GetChar() == ' ');
+    assert(e[2]->GetChar() == '6');
+    assert(e.size() == 3);
+
+    e = b.GetRowAt(2);
+    assert(e[0]->GetChar() == '7');
+    assert(e[1]->GetChar() == '8');
+    assert(e[2]->GetChar() == '9');
+    assert(e.size() == 3);
+
+    e = b.GetColumnAt(0);
+    assert(e[0]->GetChar() == '1');
+    assert(e[1]->GetChar() == '4');
+    assert(e[2]->GetChar() == '7');
+    assert(e.size() == 3);
+
+    e = b.GetColumnAt(1);
+    assert(e[0]->GetChar() == '2');
+    assert(e[1]->GetChar() == ' ');
+    assert(e[2]->GetChar() == '8');
+    assert(e.size() == 3);
+
+    e = b.GetColumnAt(2);
+    assert(e[0]->GetChar() == '3');
+    assert(e[1]->GetChar() == '6');
+    assert(e[2]->GetChar() == '9');
+    assert(e.size() == 3);
+
+    return 0;
+}
+
+int TestStaircase() {
+    Board b;
+    vector<Cell *> row0, row1, row2;
+    row0.push_back(new Cell('1'));
+    row1.push_back(new Cell('4'));    row1.push_back(new Cell('5'));
+    row2.push_back(new Cell('7'));    row2.push_back(new Cell('8'));    row2.push_back(new Cell('9'));
+    b.SetRowAt(row0, 0);
+    b.SetRowAt(row1, 1);
+    b.SetRowAt(row2, 2);
+
+    vector<Cell *> e = b.GetRowAt(0);
+    assert(e[0]->GetChar() == '1');
+    assert(e.size() == 3);
+
+    e = b.GetRowAt(1);
+    assert(e[0]->GetChar() == '4');
+    assert(e[1]->GetChar() == '5');
+    assert(e.size() == 3);
+
+    e = b.GetRowAt(2);
+    assert(e[0]->GetChar() == '7');
+    assert(e[1]->GetChar() == '8');
+    assert(e[2]->GetChar() == '9');
+    assert(e.size() == 3);
+
+    e = b.GetColumnAt(0);
+    assert(e[0]->GetChar() == '1');
+    assert(e[1]->GetChar() == '4');
+    assert(e[2]->GetChar() == '7');
+    assert(e.size() == 3);
+
+    e = b.GetColumnAt(1);
+    assert(e[0]->GetChar() == ' ');
+    assert(e[1]->GetChar() == '5');
+    assert(e[2]->GetChar() == '8');
+    assert(e.size() == 3);
+
+    e = b.GetColumnAt(2);
+    assert(e[0]->GetChar() == ' ');
+    assert(e[1]->GetChar() == ' ');
+    assert(e[2]->GetChar() == '9');
+    assert(e.size() == 3);
+
+    return 0;
+}
+
+int TestInverseStaircase() {
+    Board b;
+    vector<Cell *> row0, row1, row2;
+    row0.push_back(new Cell('1'));  row0.push_back(new Cell('2'));  row0.push_back(new Cell('3'));
+    row1.push_back(new Cell('4'));  row1.push_back(new Cell('5'));
+    row2.push_back(new Cell('7'));
+    b.SetRowAt(row0, 0);
+    b.SetRowAt(row1, 1);
+    b.SetRowAt(row2, 2);
+
+    vector<Cell *> e = b.GetRowAt(0);
+    assert(e[0]->GetChar() == '1');
+    assert(e[1]->GetChar() == '2');
+    assert(e[2]->GetChar() == '3');
+    assert(e.size() == 3);
+
+    e = b.GetRowAt(1);
+    assert(e[0]->GetChar() == '4');
+    assert(e[1]->GetChar() == '5');
+    assert(e[2]->GetChar() == ' ');
+    assert(e.size() == 3);
+
+    e = b.GetRowAt(2);
+    assert(e[0]->GetChar() == '7');
+    assert(e[1]->GetChar() == ' ');
+    assert(e[2]->GetChar() == ' ');
+    assert(e.size() == 3);
+
+    e = b.GetColumnAt(0);
+    assert(e[0]->GetChar() == '1');
+    assert(e[1]->GetChar() == '4');
+    assert(e[2]->GetChar() == '7');
+    assert(e.size() == 3);
+
+    e = b.GetColumnAt(1);
+    assert(e[0]->GetChar() == '2');
+    assert(e[1]->GetChar() == '5');
+    assert(e[2]->GetChar() == ' ');
+    assert(e.size() == 3);
+
+    e = b.GetColumnAt(2);
+    assert(e[0]->GetChar() == '3');
+    assert(e[1]->GetChar() == ' ');
+    assert(e[2]->GetChar() == ' ');
+    assert(e.size() == 3);
 
     return 0;
 }
 
 int main() {
     Test3by3Matrix();
+    Test3by3MatrixWithHole();
+    TestStaircase();
+    TestInverseStaircase();
    
     return 0;
 }
